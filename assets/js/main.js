7420782cf2daa3d4181fbdcf1f8a790c0cbe0ed4
webnazarevichi <86548830+webnazarevichi@users.noreply.github.com>
2022-06-23 18:09:20 +0300

    add two pages
    ass two pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,37 @@
 $( document ).ready(function() {
   
+    $(document).ready(function(){
+        $('.sidewalk-slider__wrapper').slick({
+            slidesToShow: 3,
+            prevArrow: '<button type="button" class="slick-prev"><img src="assets/icons/l-arrow.svg"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="assets/icons/r-arrow.svg"></button>',
+            responsive: [
+                {
+                  breakpoint: 769,
+                  settings: {
+                    slidesToShow: 2,
+                  }
+                },
+                {
+                  breakpoint: 575,
+                  settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 2000
+                  }
+                },
+
+              ]
+        });
+    });
 
 
+    $(".sidewalk-slider__item_img").magnificPopup({
+        type : "image",
+        gallery : {
+            enabled : true
+        }
+    });
 
 });
